a3002da3e13869714f2bd016e85f867ba0c293d4
gitaaulia05 <gitaauliahafid@gmail.com>
2026-01-14 00:21:17 +0700

    first commit

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShootBall : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public float shootForce = 800f;
+
+    void Update()
+    {
+        // Klik KANAN mouse (Input System)
+        if (Mouse.current != null && Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 spawnPos = transform.position + transform.forward * 0.5f;
+
+        GameObject bullet = Instantiate(
+            bulletPrefab,
+            spawnPos,
+            Quaternion.identity
+        );
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * shootForce);
+    }
+
+}
